d8fc6fcfd9703b7138b62aba7d2b14d2d10bff18
Sandunga <sandra.cuevastorres@gmail.com>
2019-06-12 17:34:52 -0500

    Nombre Animado

---
+++ src/components/Plane.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyName from '../img/myName.gif';
+import './style/bike.css'
+import Image from 'react-image-resizer';
+
+const Name = () => {
+    return (
+        <div className="imgPlane">
+            <Image src={MyName} alt="gif"width={1400} height={205}/>
+        </div>
+    )
+}
+
+export default Name;
--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -4,11 +4,12 @@ import img001 from '../img/img001.jpg';
 import img002 from '../img/img002.jpg';
 import img003 from '../img/img003.jpg';
 import BikeMe from './Bike'
+import MyName from './Plane'
 import './style/slide.css'
 
 const properties = {
-    duration:10000,
-    transitionDuration:500,
+    duration:100000,
+    transitionDuration:1000,
     infinite:true,
     indicators:true,
     arrows:true
@@ -20,8 +21,7 @@ const Slideshow = () => {
         <div className="containerSlide">
         <Slide {...properties}>
             <div className="each-slide" style={{backgroundImage: `url(${img001})`, height: '100vh', backgroundRepeat:'no-repeat'}}>
-                <div>About me</div>
-                <button>Boton</button>
+                <MyName style={{flex: 1, resizeMode: 'contain' }}/>
                 <BikeMe />
             </div>
             <div className="each-slide" style={{backgroundImage: `url(${img002})`, height: '100vh', backgroundRepeat:'no-repeat'}}>
