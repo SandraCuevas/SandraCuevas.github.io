6bfc9ffb8705e3ec9caded6fcc34ffb7dd6137b7
Sandunga <sandra.cuevastorres@gmail.com>
2019-07-22 11:36:17 -0500

    arreglando responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,10 +7,12 @@ const ContactMe = () => {
         <Container className="contact">
         <Row>
           <Col md={{ span: 3, offset: 9 }} className="contactIcon">
+          
           <Alert.Link target="_blank" href="https://www.github.com/SandraCuevas"><i className="fab fa-github-square"></i></Alert.Link>
           <Alert.Link target="_blank" href="https://www.linkedin.com/in/sandra-cuevastorres"><i class="fab fa-linkedin"></i></Alert.Link>
           <Alert.Link target="_blank" href="https://www.behance.net/zandryuxf92e"><i class="fab fa-behance-square"> </i></Alert.Link>
           <Alert.Link target="_blank" href="https://twitter.com/sandryux"><i class="fab fa-twitter-square"></i></Alert.Link>
+          <Alert.Link target="_blank" href="https://www.dropbox.com/s/svfjlu6toy78g1t/CV_SandraCuevas01.pdf?dl=0"><i class="fab fa-whatsapp-square"></i></Alert.Link>
           </Col>
         </Row>
       </Container>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Skills from '../img/Skills.gif';
 //import Sun from '../img/SolCV.png'
 import '../App.css';
-import { Container, Alert, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 const MySkills = () => {
@@ -10,26 +10,11 @@ const MySkills = () => {
 
         <Container >
             <Row>
-                <Col >
-                    <Alert.Link className="download" target="_blank" href="https://www.dropbox.com/s/svfjlu6toy78g1t/CV_SandraCuevas01.pdf?dl=0" >
-                        <br/>
-                        <br/>
-                        <br/>
-                        <i class="fas fa-address-card"></i>
-                        <br/>download<br/>
-                        MY RESUME
-                    </Alert.Link>
-                </Col>
-                
-            </Row>
-            <Row>
-              
                 <Col >   
-                    <img alt="ballons"
+                    <img className="ballons" alt="ballons"
                     src={Skills} />
                 </Col> 
             </Row>
-           
         </Container>
 
     )
